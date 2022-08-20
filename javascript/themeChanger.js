0e9028f3489f6b11c5a3cceb0577ba2f1cdda90e
John Fox <107318784+jff97@users.noreply.github.com>
2022-08-20 10:25:20 -0500

    added a radioclick function

--- javascript/themeChanger.js
+++ javascript/themeChanger.js
@@ -1,2 +1,11 @@
 //document.getElementById('textbox_id').value
 
+function radioClick(type) {
+   if (type === "default1") {
+      //do default1 stuff
+   } else if (type === "default2") {
+      //do default2 stuff
+   } else if (type === "custom") {
+      //do custom stuff
+   }
+}
