6e2f7a1a47bebcf072efb3d567e645af3ed5c3aa
John Fox <107318784+jff97@users.noreply.github.com>
2022-08-20 10:30:46 -0500

    aaaaa

--- javascript/themeChanger.js
+++ javascript/themeChanger.js
@@ -1,12 +1,13 @@
 //document.getElementById('textbox_id').value
 
 function radioClick(type) {
+   let customContainer = document.getElementById(customContainer);
    if (type == 1) {
       //do default1 stuff
+      customContainer.style
    } else if (type == 2) {
       //do default2 stuff
    } else if (type == 3) {
       //do custom stuff
-      document.getElementById(customContainer).style
    }
 }
